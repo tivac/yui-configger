1982391cbef8dabd36d34fa81de22569cc079561
Pat Cavit <pcavit@gmail.com>
2012-06-12 23:42:08 -0700

    Got simple generation working
    - Very basic functionality now working
    - Need to figure out how to re-insert comments back into generated code
      - Have some plans around that using ranges, but seems gnarly
      - May be better to use config AST to generate unique-ish regexes to do replacement

---
+++ tests/data/_config-template.js
@@ -0,0 +1,29 @@
+/*global YUI:true */
+var test_config = {
+    groups : {
+        root : {
+            /* live
+            combine   : true,
+            comboBase : "/combo/_",
+            root      : "/js/",
+            //live */
+
+            //* dev
+            //base : "/js/",
+            //dev */
+
+            modules : "configger"
+        },
+
+        subfolder : {
+            /*aliases : {
+                "home" : [
+                    "view-home",
+                    "page-home"
+                ]
+            },*/
+
+            modules : "configger"
+        }
+    }
+};
