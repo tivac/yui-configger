70bfdf597de31d2b2b9ac9bc34ed5cdb1af4ee24
Pat Cavit <pcavit@gmail.com>
2012-06-16 00:12:31 -0700

    Getting towards comments
    Tracking size of generated group configs
    Updating comment node ranges when configs are injected
    Way better use of Optimist

--- config.js
+++ config.js
@@ -1,21 +1,27 @@
 module.exports = {
     root : {
-        demand : true,
-        alias : 'r',
+        demand      : true,
+        alias       : "r",
         description : "Root directory to read YUI modules from"
     },
     tmpl : {
-        alias : 't',
-        description :
-            "YUI config file to use as template,\n" +
-            "\t\twill use _config.js in Root if not specified"
+        alias       : "t",
+        description : "YUI config file to use as template",
+        "default"   : "_config-template.js"
     },
     output : {
-        alias : "o",
+        alias       : "o",
         description : "Output file for generated config (defaults to stdout)"
     },
     filter : {
-        alias : "f",
-        description : "File-name filter (regex)"
+        alias       : "f",
+        description : "File-name filter (regex)",
+        "default"   : "."
+    },
+    key : {
+        alias       : "k",
+        description : "Key in config to replace with module metadata",
+        "default"   : "configger"
     }
+
 };
