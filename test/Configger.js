57dfbf7027be2d3079d66ee7452652d5d29819ce
Pat Cavit <pcavit@gmail.com>
2014-01-13 10:42:36 -0800

    Properly silence tests

--- test/Configger.js
+++ test/Configger.js
@@ -111,8 +111,8 @@ describe("yui-configger", function() {
         
         it("should return a config string from run (simple)", function() {
             var c = new Configger({
-                    root  : "./test/specimens/simple/",
-                    level : "silent"
+                    root   : "./test/specimens/simple/",
+                    silent : true
                 });
             
             assert.equal(
@@ -123,8 +123,8 @@ describe("yui-configger", function() {
         
         it("should return a config string from run (group-template)", function() {
             var c = new Configger({
-                    root  : "./test/specimens/group-template/",
-                    level : "silent"
+                    root   : "./test/specimens/group-template/",
+                    silent : true
                 });
                
             assert.equal(
@@ -135,8 +135,8 @@ describe("yui-configger", function() {
         
         it("should return a config string from run (standard)", function() {
             var c = new Configger({
-                    root  : "./test/specimens/standard/",
-                    level : "silent"
+                    root   : "./test/specimens/standard/",
+                    silent : true
                 });
            
             assert.equal(
@@ -172,8 +172,8 @@ describe("yui-configger", function() {
         
         it("should bail if no ast can be generated", function() {
             var c = new Configger({
-                    root  : "./test/specimens/empty/",
-                    level : "silent"
+                    root   : "./test/specimens/empty/",
+                    silent : true
                 });
                 
             assert.equal(c.run(), undefined);
