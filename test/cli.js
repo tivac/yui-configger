40caae9c0c3fb73f6c1cb09a8387b8b8793eb6e3
Pat Cavit <pcavit@gmail.com>
2014-01-15 09:41:01 -0800

    Fix CLI tests to expect stripped trailing newline
    Nixt does that, not entirely sure why but it's not the end of the world.
    
    Also removed tests that depend on the css-modules branch.

--- test/cli.js
+++ test/cli.js
@@ -5,10 +5,15 @@
 
 var fs        = require("fs"),
     nixt      = require("nixt"),
+    base      = "node bin/cli.js ",
     options   = {
         newlines : true
     },
-    base      = "node bin/cli.js ";
+    _file;
+
+_file = function(file) {
+    return fs.readFileSync(file, "utf8").replace(/\r?\n$/, "");
+};
 
 describe("yui-configger", function() {
     describe("CLI", function() {
@@ -18,7 +23,7 @@ describe("yui-configger", function() {
                 .base(base)
                 .run("--root=./test/specimens/simple/ --silent ./test/specimens/simple/")
                 .code(0)
-                .stdout(fs.readFileSync("./test/specimens/simple/_config.js", "utf8"))
+                .stdout(_file("./test/specimens/simple/_config.js"))
                 .end(done);
         });
 
@@ -27,7 +32,7 @@ describe("yui-configger", function() {
                 .base(base)
                 .run("--root=./test/specimens/group-template/ --silent ./test/specimens/group-template/")
                 .code(0)
-                .stdout(fs.readFileSync("./test/specimens/group-template/_config.js", "utf8"))
+                .stdout(_file("./test/specimens/group-template/_config.js"))
                 .end(done);
         });
         
@@ -36,28 +41,10 @@ describe("yui-configger", function() {
                 .base(base)
                 .run("--root=./test/specimens/standard/ --silent ./test/specimens/standard/")
                 .code(0)
-                .stdout(fs.readFileSync("./test/specimens/standard/_config.js", "utf8"))
+                .stdout(_file("./test/specimens/standard/_config.js"))
                 .end(done);
         });
 
-        it("should return a config string from run (mixed, no css)", function(done) {
-            nixt(options)
-                .base(base)
-                .run("--root=./test/specimens/mixed/ --silent ./test/specimens/mixed/")
-                .code(0)
-                .stdout(fs.readFileSync("./test/specimens/mixed/js/_config.js", "utf8"))
-                .end(done);
-        });
-
-        it("should return a config string from run (mixed, css)", function(done) {
-            nixt(options)
-                .base(base)
-                .run("--root=./test/specimens/mixed/ --css --silent ./test/specimens/mixed/")
-                .code(0)
-                .stdout(fs.readFileSync("./test/specimens/mixed/js/_config-css.js", "utf8"))
-                .end(done);
-        });
-        
         it("should bail if no ast can be generated", function(done) {
             nixt(options)
                 .base(base)
