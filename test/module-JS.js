a89f15aee42afb480a04abe59e56a0730eb70277
Pat Cavit <pcavit@gmail.com>
2014-02-26 07:47:08 -0800

    JS module tests

--- test/module-JS.js
+++ test/module-JS.js
@@ -1,156 +1,54 @@
 /*jshint node:true */
-/*global describe, it */
-
 "use strict";
 
-var assert = require("assert"),
-    Module = require("../lib/modules/js");
+var fs       = require("fs"),
+    assert   = require("assert"),
+    
+    JSModule = require("../lib/modules/js");
 
 describe("yui-configger", function() {
-    describe("Module Class", function() {
+    describe("JSModule Class", function() {
         it("should instantiate", function() {
-            new Module({
-                file : "./test/specimens/simple/a.js",
-                type : "js"
+            new JSModule({
+                file : "./test/specimens/not-module.js"
             });
         });
         
-        it("should require both file & type be specified", function() {
+        it("should require a file be specified", function() {
             assert.throws(function() {
-                new Module();
+                new JSModule();
             });
 
-            assert.throws(function() {
-                new Module({
+            assert.doesNotThrow(function() {
+                new JSModule({
                     file : "./test/specimens/simple/a.js"
-                });
-            });
-
-            assert.throws(function() {
-                new Module({
-                    type : "js"
                 });
             });
         });
 
         it("should read a JS file", function() {
-            var m = new Module({
-                    file : "./test/specimens/simple/a.js",
-                    type : "js"
+            var js = new JSModule({
+                    file : "./test/specimens/not-module.js"
                 });
             
-            assert(m._src);
-            assert(m._src.length > 0);
-        });
-
-        it("should read a CSS file", function() {
-            var m = new Module({
-                    file : "./test/specimens/mixed/css/a.css",
-                    type : "css"
-                });
-            
-            assert(m._src);
-            assert(m._src.length > 0);
+            assert(js._src);
+            assert(js._src.length > 0);
+            assert(js._src === fs.readFileSync("./test/specimens/not-module.js", "utf8"));
         });
         
-        it("should provide getters for simple values", function() {
-            var js = new Module({
-                    file : "./test/specimens/simple/a.js",
-                    type : "js"
+        it("should add the module's `path` to a config", function() {
+            var js = new JSModule({
+                    file : "./test/specimens/not-module.js"
                 }),
-                css = new Module({
-                    file : "./test/specimens/mixed/css/a.css",
-                    type : "css",
-                    name : "a"
-                });
-            
-            assert.equal(js.file, "./test/specimens/simple/a.js");
-            assert.equal(js.type, "js");
-            assert.equal(js.name, "module-a");
-            assert(js.config);
+                config = {
+                    properties : [ "fooga" ]
+                };
 
-            assert.equal(css.file, "./test/specimens/mixed/css/a.css");
-            assert.equal(css.type, "css");
-            assert.equal(css.name, "a");
-            assert(css.config);
-        });
+            js._addPath(config);
 
-        it("should provide a `valid` getter to check validity", function() {
-            var js = new Module({
-                    file : "./test/specimens/simple/a.js",
-                    type : "js"
-                }),
-                css = new Module({
-                    file : "./test/specimens/mixed/css/a.css",
-                    type : "css"
-                });
-
-            assert(js.valid());
-            assert(css.valid());
-        });
-
-        it("should not generate config if passed an invalid file", function() {
-            assert.throws(function() {
-                new Module({
-                    file : "./test/specimens/simple/fooga.js",
-                    type : "js"
-                });
-            });
-        });
-        
-        it("should not generate config if file is not a YUI modules (JS)", function() {
-            var m = new Module({
-                    file : "./test/specimens/not-module.js",
-                    type : "js"
-                });
-            
-            assert.equal(m.config, undefined);
-        });
-
-        it("should not generate config if no name is specified (JS)", function() {
-            var m = new Module({
-                    file : "./test/specimens/no-name.js",
-                    type : "js"
-                });
-            
-            assert.equal(m.config, undefined);
-        });
-        
-        it("should not generate config if esprima cannot create an AST (JS)", function() {
-            var m = new Module({
-                    file : "./test/specimens/invalid.js",
-                    type : "js"
-                });
-            
-            assert.equal(m.config, undefined);
-        });
-
-        it("should not be valid if an AST cannot be created", function() {
-            var m = new Module({
-                    file : "./test/specimens/invalid.js",
-                    type : "js"
-                });
-
-            assert(!m.valid());
-        });
-        
-        it("should use a default module config if one isn't defined", function() {
-            var js = new Module({
-                    file : "./test/specimens/simple/c.js",
-                    type : "js"
-                }),
-                css = new Module({
-                    file : "./test/specimens/mixed/css/a.css",
-                    type : "css"
-                });
-            
-            assert.equal(js.config.type, "ObjectExpression");
-            assert(js.config.properties.length);
-            assert(js.config.properties[0].value.value, "c.js");
-
-            assert.equal(css.config.type, "ObjectExpression");
-            assert(css.config.properties.length);
-            assert(css.config.properties[0].value.value, "a.css");
+            assert(config.properties.length === 2);
+            assert(config.properties[1] === "fooga");
+            assert(config.properties[0].value.value = "not-module.js");
         });
     });
 });
