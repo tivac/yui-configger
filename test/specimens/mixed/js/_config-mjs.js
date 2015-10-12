b17d5c255de14fb26f3c8ccc9e143859eb1f83bd
Pat Cavit <pcavit@gmail.com>
2015-10-12 14:56:47 -0700

    Clean up specimens to fix tests
    Mostly indentation fixes, a few minor AST tweaks as well.

--- test/specimens/mixed/js/_config-mjs.js
+++ test/specimens/mixed/js/_config-mjs.js
@@ -1,14 +1,14 @@
 var test_config = {
-        groups: {
-            "/js/templates/": {
-                base: "/js/templates/",
-                modules: {
-                    "template-a": { path: "a.mjs" },
-                    "template-b": {
-                        path: "b.mjs",
-                        requires: ["template-a"]
-                    }
+    groups: {
+        "/js/templates/": {
+            base: "/js/templates/",
+            modules: {
+                "template-a": { path: "a.mjs" },
+                "template-b": {
+                    path: "b.mjs",
+                    requires: ["template-a"]
                 }
             }
         }
-    };
+    }
+};
--- test/specimens/standard/_config.js
+++ test/specimens/standard/_config.js
@@ -1,113 +1,113 @@
 var test_config = {
-        groups: {
-            "/subfolder-b/sub-subfolder/": {
-                base: "/subfolder-b/sub-subfolder/",
-                modules: { "module-j": { path: "j.js" } }
-            },
-            "/subfolder-b/sub-subfolder/sub-sub-subfolder/": {
-                base: "/subfolder-b/sub-subfolder/sub-sub-subfolder/",
-                modules: { "module-k": { path: "k.js" } }
-            },
-            "/": {
-                comboBase: "/combo/_",
-                root: "/",
-                base: "/",
-                modules: {
-                    "module-a": {
-                        path: "a.js",
-                        requires: [
-                            "view-base",
-                            "extension-view-classer",
-                            "extension-view-purchasing",
-                            "extension-view-tooltips",
-                            "transform-imagesizer",
-                            "transform-buttondir",
-                            "transform-idattribute",
-                            "gallery-event-visible",
-                            "gw2-template-items",
-                            "item-templates",
-                            "gw2-i18n-gemstore-items"
-                        ]
-                    },
-                    "module-b": {
-                        path: "b.js",
-                        requires: [
-                            "node",
-                            "model"
-                        ]
-                    },
-                    "module-c": {
-                        path: "c.js",
-                        requires: [
-                            "io-base",
-                            "handlebars-base"
-                        ]
-                    },
-                    "module-d": { path: "d.js" }
-                }
-            },
-            "test-root": {
-                base: "/js/",
-                modules: "configger"
-            },
-            "/subfolder/": {
-                aliases: {
-                    "home": [
-                        "view-home",
-                        "page-home"
+    groups: {
+        "/subfolder-b/sub-subfolder/": {
+            base: "/subfolder-b/sub-subfolder/",
+            modules: { "module-j": { path: "j.js" } }
+        },
+        "/subfolder-b/sub-subfolder/sub-sub-subfolder/": {
+            base: "/subfolder-b/sub-subfolder/sub-sub-subfolder/",
+            modules: { "module-k": { path: "k.js" } }
+        },
+        "/": {
+            comboBase: "/combo/_",
+            root: "/",
+            base: "/",
+            modules: {
+                "module-a": {
+                    path: "a.js",
+                    requires: [
+                        "view-base",
+                        "extension-view-classer",
+                        "extension-view-purchasing",
+                        "extension-view-tooltips",
+                        "transform-imagesizer",
+                        "transform-buttondir",
+                        "transform-idattribute",
+                        "gallery-event-visible",
+                        "gw2-template-items",
+                        "item-templates",
+                        "gw2-i18n-gemstore-items"
                     ]
                 },
-                modules: {
-                    "module-e": {
-                        path: "e.js",
-                        requires: [
-                            "view-base",
-                            "extension-view-classer",
-                            "extension-view-purchasing",
-                            "extension-view-tooltips",
-                            "transform-imagesizer"
-                        ]
-                    },
-                    "module-f": {
-                        path: "f.js",
-                        requires: [
-                            "node",
-                            "modellist",
-                            "base"
-                        ]
-                    }
-                }
+                "module-b": {
+                    path: "b.js",
+                    requires: [
+                        "node",
+                        "model"
+                    ]
+                },
+                "module-c": {
+                    path: "c.js",
+                    requires: [
+                        "io-base",
+                        "handlebars-base"
+                    ]
+                },
+                "module-d": { path: "d.js" }
+            }
+        },
+        "test-root": {
+            base: "/js/",
+            modules: "configger"
+        },
+        "/subfolder/": {
+            aliases: {
+                "home": [
+                    "view-home",
+                    "page-home"
+                ]
             },
-            "/subfolder-b/": {
-                modules: {
-                    "module-g": {
-                        path: "g.js",
-                        requires: [
-                            "view-base",
-                            "extension-view-classer",
-                            "extension-view-purchasing",
-                            "extension-view-tooltips",
-                            "transform-imagesizer"
-                        ]
-                    },
-                    "module-g": {
-                        path: "h.js",
-                        requires: [
-                            "node",
-                            "modellist",
-                            "base"
-                        ]
-                    },
-                    "module-i": {
-                        path: "i.js",
-                        requires: [
-                            "node",
-                            "modellist",
-                            "base"
-                        ]
-                    }
+            modules: {
+                "module-e": {
+                    path: "e.js",
+                    requires: [
+                        "view-base",
+                        "extension-view-classer",
+                        "extension-view-purchasing",
+                        "extension-view-tooltips",
+                        "transform-imagesizer"
+                    ]
+                },
+                "module-f": {
+                    path: "f.js",
+                    requires: [
+                        "node",
+                        "modellist",
+                        "base"
+                    ]
                 }
-            },
-            "test-subfolder": { modules: "configger" }
-        }
-    };
+            }
+        },
+        "/subfolder-b/": {
+            modules: {
+                "module-g": {
+                    path: "g.js",
+                    requires: [
+                        "view-base",
+                        "extension-view-classer",
+                        "extension-view-purchasing",
+                        "extension-view-tooltips",
+                        "transform-imagesizer"
+                    ]
+                },
+                "module-g": {
+                    path: "h.js",
+                    requires: [
+                        "node",
+                        "modellist",
+                        "base"
+                    ]
+                },
+                "module-i": {
+                    path: "i.js",
+                    requires: [
+                        "node",
+                        "modellist",
+                        "base"
+                    ]
+                }
+            }
+        },
+        "test-subfolder": { modules: "configger" }
+    }
+};
